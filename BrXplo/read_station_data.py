8a6dc79404d108bab217f8e7bf534e58602419e4
ziu1986 <Stefanie.Falk@geo.uio.no>
2019-02-18 11:15:42 +0100

    Updated reading of NOAA files to handle a broughter variaty.

--- BrXplo/read_station_data.py
+++ BrXplo/read_station_data.py
@@ -4,10 +4,10 @@ from mytools.met_tools import *
 from mytools.netcdf_tools import * # ncdump implementation for python
 
 def read_station_data(infile):
-    import nappy as nap # Read and write NASA data files
     '''
     Conversion of NASA aimes files from nilo.no data sets.
     '''
+    import nappy as nap # Read and write NASA data files
     # Open file (read header only)
     nas_file = nap.openNAFile(infile)
     # Read actual data
@@ -70,12 +70,12 @@ def read_station_data_noaa(infile, **kargs):
     '''
     import codecs
     # Key words
-    start_data = kargs.pop('start_data')
+    start_data = kargs.pop('start_data', 0)
     utc = kargs.pop('utc', 0)
     column = kargs.pop('column', 2)
     station = kargs.pop('station', 'standard')
     # Reading data
-    lines = codecs.open(infile, "r", 'utf-8').readlines()
+    lines = codecs.open(infile, "r", 'utf-8').read().splitlines()
     # Compute the timeshift
     timeshift = dt.timedelta(hours=utc)
     # Data structure
@@ -87,10 +87,17 @@ def read_station_data_noaa(infile, **kargs):
         # Seperate ozone data
         ozone_data_raw.append(float(col[column]))
         # Generate timestamp
-        if station=='southpole':
-            time.append(dt.datetime.strptime("%s-%s-%s %s" % (col[1], col[2], col[3], col[4]), '%Y-%m-%d %H')-timeshift)
+        if (station=='southpole') | (station=='other'):
+            if  int(col[4]) == 24:
+                temp_time = dt.datetime.strptime("%s-%s-%s %s" % (col[1], col[2], col[3], '00'), '%Y-%m-%d %H')+dt.timedelta(days=1)
+            else:
+                temp_time = dt.datetime.strptime("%s-%s-%s %s" % (col[1], col[2], col[3], col[4]), '%Y-%m-%d %H')
         else:
-            time.append(dt.datetime.strptime("%s" % (col[0]+" "+col[1]), '%Y-%m-%d %H:%M')-timeshift)
+            if int(col[1][:2]) == 24:
+                temp_time = dt.datetime.strptime("%s" % (col[0]+" 00:00"), '%Y-%m-%d %H:%M')+dt.timedelta(days=1)
+            else:
+                temp_time = dt.datetime.strptime("%s" % (col[0]+" "+col[1]), '%Y-%m-%d %H:%M')
+        time.append(temp_time-timeshift)
     # Mask the data (negative ozone values)
     ozone_data = np.ma.masked_where(np.array(ozone_data_raw)<0, np.array(ozone_data_raw))
     x_time = np.ma.masked_where(np.array(ozone_data_raw)<0, np.array(time))
