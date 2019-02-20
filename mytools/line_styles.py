b8a3bffdb9b7a8d175f6a80c487089aee2429d2a
ziu1986 <Stefanie.Falk@geo.uio.no>
2019-02-20 11:00:56 +0100

    Corrected spelling mistake and made linestyles more distinctive.

--- mytools/line_styles.py
+++ mytools/line_styles.py
@@ -1,2 +1,2 @@
-# costomized line styles
-cost_linestyle = {"s-":(4,2), "-.":(8,4,2,4), "-..":(8,4,2,4,2,4), "_.":(12,8,2,8)}
+# customized line styles
+custom_linestyle = {"s-":(3,1), "-.":(8,4,2,4), "-..":(8,4,2,4,2,4), "_.":(12,8,2,8)}
