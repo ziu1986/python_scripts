c4bbad5f45e94b03d4b01c8ec786cb5f930c0970
ziu1986 <Stefanie.Falk@geo.uio.no>
2019-03-04 16:50:42 +0100

    Found out that windrose takes meteorological wind directions, have corrected model wind directions and placed a lower threashold on observed wind. Plots and script still need some fine tuning.

--- ozone_metrics/analyse_wind.py
+++ ozone_metrics/analyse_wind.py
@@ -10,9 +10,14 @@ from windrose import WindroseAxes # Drawing wind roses
 from mytools.met_tools import print_all
 from station_info import station_location
 
-def hist_hist_diagram(wd, ws, title, xlabel, ylabel):
+def hist_hist_diagram(wd, ws, title, xlabel, ylabel, fig1, **kwargs):
+    model = kwargs.pop("model", True)
+    if model:
+        wd[np.where(wd>360)] = wd[np.where(wd>360)]-360
+    bin_range_wd = range(0,361)
+    bin_range_ws = np.arange(-0.05, 13.1, 0.1)
     
-    # definitions for the axes
+    # Definitions for the axes
     left, width = 0.1, 0.65
     bottom, height = 0.1, 0.65
     bottom_h = left_h = left + width + 0.02
@@ -25,16 +30,15 @@ def hist_hist_diagram(wd, ws, title, xlabel, ylabel):
     axHistx = plt.axes(rect_histx)
     axHisty = plt.axes(rect_histy)
     
-    fig1 = plt.figure(1)
     fig1.canvas.set_window_title(title)
 
     nullfmt = NullFormatter()         # no labels
     axHistx.xaxis.set_major_formatter(nullfmt)
     axHisty.yaxis.set_major_formatter(nullfmt)
 
-    axScatter.hist2d(wd, ws, bins=(range(-180,180),np.arange(0,10.1,0.1)), cmap=plt.cm.hot_r)
-    axHistx.hist(wd, bins=range(-180,180), normed=True)
-    axHisty.hist(ws, bins=np.arange(0,25,0.1), normed=True, orientation='horizontal')
+    axScatter.hist2d(wd, ws, bins=(bin_range_wd, bin_range_ws), cmap=plt.cm.hot_r)
+    axHistx.hist(wd, bins=bin_range_wd, normed=True)
+    axHisty.hist(ws, bins=bin_range_ws, normed=True, orientation='horizontal')
 
     # Adjust the axes ranges
     axHistx.set_xlim(axScatter.get_xlim())
@@ -42,24 +46,37 @@ def hist_hist_diagram(wd, ws, title, xlabel, ylabel):
     # Decorate the plot
     axScatter.set_xlabel(xlabel)
     axScatter.set_ylabel(ylabel)
+    axScatter.set_title("%s" % (title[:title.find('_')]), y=1.15, x=1.1, size='xx-large')
+    axScatter.text(87,12.7,"E", size='x-large')
+    axScatter.text(177,12.7,"S", size='x-large')
+    axScatter.text(267,12.7,"W", size='x-large')
+    axScatter.text(0,12.7,"N", size='x-large')
+    axScatter.text(353,12.7,"N", size='x-large')
+    axScatter.set_xticks(np.arange(0,361,45))
+    axHistx.set_xticks(np.arange(0,361,45))
     axHistx.set_ylabel("Probability density")
     axHisty.set_xlabel("Probability density")
 
-def windrose(wd, ws, title):
-    fig2 = plt.figure(2)
+def windrose(wd, ws, title, fig2, **kwargs):
+    model = kwargs.pop('model', True)
     fig2.canvas.set_window_title(title)
+
+    if model:
+        wd[np.where(wd>360)] = wd[np.where(wd>360)]-360
     # Shift wind directions [-180:180] -> [0:360]
-    wd[np.where(wd<0)] = 360+wd[np.where(wd<0)]
     ax21 = WindroseAxes.from_ax(fig=fig2)
     ax21.bar(wd, ws, normed=True, opening=0.8, edgecolor='white', cmap=plt.cm.viridis)
     ax21.set_legend()
-    
+    ax21.set_title("%s" % (title[:title.find('_')]), y=0.98, x=0., size='xx-large')
+
+
 # Closing plots from previous runs
 plt.close('all')
 
 # Path to the file
 #src_dir = os.environ['DATA'] + '/CTM3_input_data' + '/metdataOpenIFS/cy38r1nc4_2012/T159N80L60/*/*.nc'
 src_dir = os.environ['HOME'] +"/bin/wind*1990.nc"
+src_dir_obs = os.environ['DATA'] + '/astra_data/observations/wind/Svanvik_2018.txt'
 
 # Read the data only once in interactive mode
 try:
@@ -84,22 +101,42 @@ except NameError:
         # Select an area
         u10 = u10.sel(lat=slice(67.6,71.4),lon=slice(19.4,31.4))
         v10 = v10.sel(lat=slice(67.6,71.4),lon=slice(19.4,31.4))
-selection = "Pallas"
+
+# Observation data
+data_obs = pd.read_csv(src_dir_obs,';', header=13)
+
+# Select station
+selection = "Svanvik"
 u10_selection = u10.sel(lat=station_location[selection].lat, lon=station_location[selection].lon, method='nearest')
 v10_selection = v10.sel(lat=station_location[selection].lat, lon=station_location[selection].lon, method='nearest')
 # Plot it
-X = (np.arctan2(v10, u10)/np.pi*180).data.ravel()
-Y = (np.sqrt(u10**2+v10**2)).data.ravel()
-wd_selection = (np.arctan2(v10_selection, u10_selection)/np.pi*180).data.ravel()
+wd = 270-(np.arctan2(v10, u10)/np.pi*180).data.ravel()
+ws = (np.sqrt(u10**2+v10**2)).data.ravel()
+
+wd_selection = 270-(np.arctan2(v10_selection, u10_selection)/np.pi*180).data.ravel()
 ws_selection = (np.sqrt(u10_selection**2+v10_selection**2)).data.ravel()
 
+
+ 
 xlabel = "Wind direction (deg)"
 ylabel = "Wind strength ($ms^{-1}$)"
 
-#hist_hist_diagram(X, Y, "fennoscandic_wind", xlabel, ylabel)
-hist_hist_diagram(wd_selection, ws_selection, "%s_wind" % selection, xlabel, ylabel)
-#windrose(X, Y, "fennoscandic_windrose")
-windrose(wd_selection, ws_selection, "%s_windrose" % selection)
+fig1 = plt.figure(1)
+#hist_hist_diagram(wd, ws, "fennoscandic_wind", xlabel, ylabel, fig1)
+hist_hist_diagram(wd_selection, ws_selection, "%s_wind" % selection, xlabel, ylabel, fig1)
+
+fig2 = plt.figure(2)
+#windrose(wd, ws, "fennoscandic_windrose")
+windrose(wd_selection, ws_selection, "%s_windrose" % selection, fig2)
+
+
+fig3 = plt.figure(3)
+hist_hist_diagram(data_obs['DD'].where(data_obs['FF']>0.8), data_obs['FF'].where(data_obs['FF']>0.8), "%s_obs_wind" % selection, xlabel, ylabel, fig3, model=False)
+
+
+fig4 = plt.figure(4)
+windrose(data_obs['DD'].where(data_obs['FF']>0.8), data_obs['FF'].where(data_obs['FF']>0.8), "%s_obs_windrose" % selection, fig4, model=False)
+
 
 # Show it
 plt.show(block=False)
