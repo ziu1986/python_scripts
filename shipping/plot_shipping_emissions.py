5cf5dd6aaaf8770e367725af18b32d27d9f08b45
ziu1986 <Stefanie.Falk@geo.uio.no>
2019-09-05 10:43:53 +0200

    Added plotting of ssp858 emission scenario and eclipse.

--- shipping/plot_shipping_emissions.py
+++ shipping/plot_shipping_emissions.py
@@ -3,14 +3,43 @@ import numpy as np
 import pandas as pd
 import xarray as xr
 import matplotlib.pyplot as plt   # Plotting data
-import cartopy as cp        # Globe projections
-import cartopy.util as ccrs_util  # Add cyclic
-from cartopy.mpl.ticker import LongitudeFormatter, LatitudeFormatter
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+import cartopy.io.img_tiles as cimgt
+#import cartopy.geodesic as cgeo
+from matplotlib.transforms import offset_copy
 from scipy.constants import *     # Get physics constants
 from mytools.met_tools import *
 
+
 # Data source
 nc_src = os.environ['DATA']+'/CTM3_input_data/EMIS/ECLIPSE_V5a/ship_CLE_emis_*.nc'
+nc_src_historic = os.environ['DATA']+'/CTM3_input_data/EMIS/CEDS0517/CEDS_CICERO/'
+nc_src_ssp585 = os.environ['DATA']+'/astra_data/input_data/ctm_input/EMIS/CEDS_SCENARIOS/SSP585/'
+var = ('SO2_em_anthro',
+       'NOx_em_anthro', 
+       'CO_em_anthro', 
+       #'CO2_em_anthro', 
+       'NH3_em_anthro', 
+       'VOC02_ethane_em_speciated_VOC_anthro', 
+       'VOC03_propane_em_speciated_VOC_anthro', 
+       'VOC04_butanes_em_speciated_VOC_anthro', 
+       'VOC05_pentanes_em_speciated_VOC_anthro', 
+       'VOC06_hexanes_pl_em_speciated_VOC_anthro', 
+       'VOC07_ethene_em_speciated_VOC_anthro', 
+       'VOC08_propene_em_speciated_VOC_anthro', 
+       'VOC13_benzene_em_speciated_VOC_anthro', 
+       'VOC14_toluene_em_speciated_VOC_anthro', 
+       'VOC15_xylene_em_speciated_VOC_anthro', 
+       'VOC16_trimethylb_em_speciated_VOC_anthro', 
+       'VOC17_other_arom_em_speciated_VOC_anthro', 
+       'VOC21_methanal_em_speciated_VOC_anthro', 
+       'VOC22_other_alka_em_speciated_VOC_anthro', 
+       'VOC23_ketones_em_speciated_VOC_anthro', 
+       'BC_em_FOSSIL_FUEL_anthro', 
+       'BC_em_SOLID_BIOFUEL_anthro', 
+       'OC_em_FOSSIL_FUEL_anthro', 
+       'OC_em_SOLID_BIOFUEL_anthro')
 
 try:
     data
@@ -19,92 +48,115 @@ except NameError:
     # Open dataset
     for file in sorted(glob.glob(nc_src)):
         print(file)
-        data = xr.open_dataset(file)
-        data_list.append(data)
-# Concatinating the listq
-data = xr.concat(data_list, dim='time')
+        tmp_data = xr.open_dataset(file)
+        data_list.append(tmp_data)
+    # Concatinating the list
+    data = xr.concat(data_list, dim='time')
+    # Reding CEDs datasets
+    data_scandic_slice_ceds = []
+    data_finnmark_slice_ceds = []
+    for ivar in var[:4]:
+        tmp_list = []
+        # Reading the historical data
+        for file in sorted(glob.glob(nc_src_historic+ivar.replace('_','-')+'*201412*.nc')):
+            print(os.path.basename(file))
+            tmp_data = xr.open_dataset(file)
+            # Defining new time coordinates
+            tmp_data['time'].reset_coords(drop=True)
+            tmp_data.coords['time'] = [dt.datetime(iyear, imonth, iday) for iyear, imonth, iday in zip(tmp_data.time.dt.year, tmp_data.time.dt.month, tmp_data.time.dt.day)]            
+            # Get gridarea (there is supposed to be a file on the CEDS servers)
+            try:
+                gridarea
+            except NameError:
+                gridarea = tmp_data["Gridcell area"]
+            tmp_list.append(tmp_data[ivar+'_SHI'])
+        # Reading the scenario data
+        for file in sorted(glob.glob(nc_src_ssp585+ivar.replace('_','-')+'*12.nc')):
+            print(os.path.basename(file))
+            tmp_data = xr.open_dataset(file)
+            # Defining new time coordinates
+            tmp_data['time'].reset_coords(drop=True)
+            tmp_data.coords['time'] = [dt.datetime(iyear, imonth, iday) for iyear, imonth, iday in zip(tmp_data.time.dt.year, tmp_data.time.dt.month, tmp_data.time.dt.day)] 
+            tmp_list.append(tmp_data[ivar+'_SHI'].drop('sector'))
+        # Concat the species and ad it to list
+        tmp_data = xr.concat(tmp_list, dim='time')
+        data_scandic_slice_ceds.append((tmp_data*gridarea).sel(lat=slice(0,40),lon=slice(56,81),drop=True))
+        data_finnmark_slice_ceds.append((tmp_data*gridarea).sel(lat=slice(16,33),lon=slice(66,71),drop=True))
+        #data_list.append(tmp_data)
 
-#Plotting
+    # Select Scandinavia
+    data_scandic_slice = data.sel(lat=slice(0,40),lon=slice(56,81),drop=True)
+    data_finnmark_slice = data.sel(lat=slice(16,33),lon=slice(66,71),drop=True)
+    x_scandic, y_scandic = [0,40,40,0,0], [56,56,81,81,56]
+    x, y = [16,33,33,16,16], [66,66,71,71,66]
+
+# Get seconds in month for all years
+try:
+    siy
+except NameError:
+    siy = []
+    for iyear in np.unique(data_scandic_slice_ceds[0].time.dt.year):
+        siy.append([seconds_in_month(imonth, iyear) for imonth in np.arange(1,13)]) 
+
 # Clean-up
 plt.close('all')
-fig1 = plt.figure(1, figsize=(16,9))
-fig1.canvas.set_window_title("total_shipping_emissions")
-for i, key in enumerate(data.data_vars):
-    plt.subplot(3,3,i+1)
-    data.sum(dim='lon')[key].transpose().plot(cmap=plt.cm.afmhot_r)
 
-for ax in fig1.axes[::2]:
-    ax.set_xlabel('')
-    ax.set_ylabel('')
-fig1.axes[6].set_ylabel('Latitude (deg)')
-fig1.axes[14].set_xlabel('Time (year)')
+#Plotting
+
+fig1 = plt.figure(1, figsize=(7,14))
+fig1.canvas.set_window_title("map_scandic_domain")
+stamen_terrain = cimgt.Stamen('terrain-background')
+#ax11 = fig1.add_subplot(1, 1, 1, projection=ccrs.PlateCarree())
+ax11 = fig1.add_subplot(1,1,1, projection=stamen_terrain.crs) #ccrs.PlateCarree()
+#ax41.set_extent([18,31,66,70], crs=ccrs.PlateCarree())
+ax11.set_extent([0,40,56,81], crs=ccrs.PlateCarree())
+# Add the Stamen data at zoom level 8.
+ax11.add_image(stamen_terrain, 4)
+#ax11.stock_img()
+ax11.coastlines(resolution='10m')
+
+# Mark the inner domain
+ax11.fill(x, y, color='coral', alpha=0.4, transform=ccrs.Geodetic())
 
 fig2 = plt.figure(2, figsize=(16,9))
-fig2.canvas.set_window_title("relative1990_shipping_emissions")
-for i, key in enumerate(data.data_vars):
-    plt.subplot(3,3,i+1)
-    ((data.sum(dim='lon')[key]-data.isel(time=0).sum(dim='lon')[key])/data.isel(time=0).sum(dim='lon')[key]).transpose().plot(cmap=plt.cm.seismic)
+fig2.canvas.set_window_title("accumulated_shipping_emissions_ECLIPSE")
+#ax21 = plt.subplot()
 
-for ax in fig2.axes[::2]:
-    ax.set_xlabel('')
-    ax.set_ylabel('')
-fig2.axes[6].set_ylabel('Latitude (deg)')
-fig2.axes[14].set_xlabel('Time (year)')
+for i, key in enumerate(data.data_vars):
+    ax = plt.subplot(3,3,i+1)
+    (100*(data_scandic_slice[key].sum(dim=('lat','lon'))-data_scandic_slice[key].sum(dim=('lat','lon')).sel(time=2005))/data_scandic_slice[key].sum(dim=('lat','lon')).sel(time=2005)).plot(ax=ax, label="Fennoscandia")
+    (100*(data_finnmark_slice[key].sum(dim=('lat','lon'))-data_finnmark_slice[key].sum(dim=('lat','lon')).sel(time=2005))/data_finnmark_slice[key].sum(dim=('lat','lon')).sel(time=2005)).plot(ax=ax, label="Cap of the North")
+    ax.set_xlabel("")
+    ax.set_ylabel("")
+    #ax.set_ylabel("%s (%s)" % (key, data_scandic_slice[key].units))
+    ax.set_title("%s" % (key), y=0.85, x=0.15)
+
+ax28 = fig2.axes[-2]
+ax24 = fig2.axes[3]
+ax28.legend(bbox_to_anchor=(0.35, -0.39), loc=8, borderaxespad=0.,ncol=4)
+ax28.set_xlabel("Time (years)")
+ax24.set_ylabel("Emissions relative to 2005 (%)") #(%s)" % (data_scandic_slice[key].units))
 
 fig3 = plt.figure(3, figsize=(16,9))
-fig3.canvas.set_window_title("zonalave_relative1990_shipping_emissions")
-for i, key in enumerate(data.data_vars):
-    plt.subplot(3,3,i+1)
-    ((data.mean(dim='lon')[key]-data.isel(time=0).mean(dim='lon')[key])/data.isel(time=0).mean(dim='lon')[key]).sel(lat=68,method='nearest').plot(label='%d$^\circ$N'%(data['lat'].sel(lat=68,method='nearest').data))
-    ((data.mean(dim='lon')[key]-data.isel(time=0).mean(dim='lon')[key])/data.isel(time=0).mean(dim='lon')[key]).sel(lat=77,method='nearest').plot(label='%d$^\circ$N'%(data['lat'].sel(lat=77,method='nearest').data))
-    ((data.mean(dim='lon')[key]-data.isel(time=0).mean(dim='lon')[key])/data.isel(time=0).mean(dim='lon')[key]).sel(lat=52,method='nearest').plot(label='%d$^\circ$N'%(data['lat'].sel(lat=52,method='nearest').data))
+fig3.canvas.set_window_title("accumulated_shipping_emissions_CEDS")
+for i in np.arange(len(data_scandic_slice_ceds)):
+    ax = plt.subplot(2,2,i+1)
+    #((data_scandic_slice_ceds[i].sum(dim=('lat','lon'))*1e-3)*np.array(siy).ravel()).plot(ax=ax)
+    #((data_finnmark_slice_ceds[i].sum(dim=('lat','lon'))*1e-3)*np.array(siy).ravel()).plot(ax=ax)
+    ref_scandic = data_scandic_slice_ceds[i].sum(dim=('lat','lon')).groupby("time.year").mean().sel(year=2005)
+    ref_finnmark = data_finnmark_slice_ceds[i].sum(dim=('lat','lon')).groupby("time.year").mean().sel(year=2005)
+    (100*(data_scandic_slice_ceds[i].sum(dim=('lat','lon'))-ref_scandic)/ref_scandic).plot(ax=ax, marker='x', ls='None', label="Fennoscandia")
+    (100*(data_finnmark_slice_ceds[i].sum(dim=('lat','lon'))-ref_finnmark)/ref_finnmark).plot(ax=ax, marker='+', ls='None', label="Cap of the North")
     
-    ax = plt.gca()
-    
-    ax.set_title(key,y=0.85,x=0.1)
-
-for ax in fig3.axes:
+    ax.set_title(var[i].replace('_em_anthro',''), y=0.85, x=0.1)
     ax.set_xlabel('')
-    ax.set_ylim(-10,50)
-    ax.set_ylabel('')
-fig3.axes[3].set_ylabel('(X(t)-X(1990))/X(1990)')
-fig3.axes[7].set_xlabel('Time (year)')
-fig3.axes[0].legend()
-
-fig6 = plt.figure(6)
-fig6.canvas.set_window_title("map_norsca")
-plat = (data.lat.sel(lat=68,method='nearest').data, data.lat.sel(lat=77,method='nearest').data)
-
-
-for i, key in enumerate(data.data_vars):
-    minimum = (data.isel(time=10)-data.isel(time=0))[key].min()
-    plt.subplot(3,3,i+1,projection=cp.crs.NorthPolarStereo())
-    if minimum >= 0:
-        (data.isel(time=10)-data.isel(time=0))[key].plot(transform=cp.crs.PlateCarree(), cmap=plt.cm.OrRd, vmin=0,
-                                                         cbar_kwargs={'label':' %s (kt)' % (key),
-                                                                      'orientation':'vertical',
-                                                                      #'fraction':0.046,
-                                                                      #'pad':0.04,
-                                                                      #'aspect':30
-                                                         })
-    else:
-        (data.isel(time=10)-data.isel(time=0))[key].plot(transform=cp.crs.PlateCarree(),
-                                                         cbar_kwargs={'label':' %s (kt)' % (key),
-                                                                      'orientation':'vertical',
-                                                                      #'fraction':0.046,
-                                                                      #'pad':0.04,
-                                                                      #'aspect':30
-                                                         })
     
-    
-for ax in fig6.axes[::2]:
-    ax.set_extent([0, 22, 56, 80], cp.crs.PlateCarree())
-    # Adding lines
-    draw_parallels(ax, np.arange(60,91,5))
-    draw_meridians(ax, np.arange(-180,181,45))    
-    ax.coastlines(resolution='50m')
-    ax.plot(np.linspace(0,180,100),np.repeat(plat[0], 100),transform=cp.crs.PlateCarree(),color='blue')
-    ax.plot(np.linspace(0,180,100),np.repeat(plat[1], 100),transform=cp.crs.PlateCarree(),color='red')
-        
+
+ax33 = fig3.axes[-2]
+ax33.set_xlabel("Time (years)", x=1.15)
+ax33.set_ylabel("Emissions relative to 2005 (%)", y=1.15)
+fig3.axes[0].legend(loc="upper right", borderaxespad=0.,ncol=4)
+
+
 # Show it
 plt.show(block=False)
