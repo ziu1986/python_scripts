8d1e64ee95843b1078ecb6735bee12efd07c2b4c
ziu1986 <Stefanie.Falk@geo.uio.no>
2019-11-29 17:01:05 +0100

    Read-in routine for the values from Xu paper.

--- plant_model/sample_from_norm.py
+++ plant_model/sample_from_norm.py
@@ -1,7 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-   
+def digitize(a, scale_x, pixel_x):
+    m = (scale_x[1]-scale_x[0])/(pixel_x[1]-pixel_x[0])
+    b = scale_x[0]-m*pixel_x[0]
+    return(m*a+b)
+
+def read_dat(path):
+    lines = open(path).readlines()
+    for line in lines[:10]:
+        if line[0].find("#")<0:
+            var_name, scale_x0, scale_x1, pixel_x0, pixel_x1 = line.split()
+            
+    for line in lines[11:]:
+        if ((line[0].find("#")<0) and (len(line)>1)):
+            exp_name, o3_mean, o3_sigma, gs_mean, gs_sigma, A_mean, A_sigma, Rd_mean, Rd_sigma, Ci_mean, Ci_sigma, Vcmax_mean, Vcmax_sigma, Jmax_mean, Jmax_sigma = line.split()
+    scale = {"var_name":var_name, "scale_x0":scale_x0, "scale_x1":scale_x1, "pixel_x0":pixel_x0, "pixel_x1":pixel_x1}
+    value = {"exp_name":exp_name, "o3_mean":o3_mean, "o3_sigma":o3_sigma, "gs_mean":gs_mean, "gs_sigma":gs_sigma, "A_mean":A_mean, "A_sigma":A_sigma, "Rd_mean":Rd_mean, "Rd_sigma":Rd_sigma, "Ci_mean":Ci_mean, "Ci_sigma":Ci_sigma, "Vcmax_mean":Vcmax_mean, "Vcmax_sigma":Vcmax_sigma, "Jmax_mean":Jmax_mean, "Jmax_sigma":Jmax_sigma}
+
+    return (scale, value)
+            
 # Clean up
 plt.close('all')
 # Constants
@@ -14,8 +32,7 @@ xu_o3_mu = np.array((32.8,29.1,27.4,25.1))
 xu_o3_sigma = np.array((1.2,1.0,0.8,0.5))
 xu_o3_label = np.array(("S1", "S2", "S3", "S4"))
 xu_o3_days = np.array((26,26,26,54))
-m = 0.5/(435.59-302.41)
-b = -302.41*m
+
 xu_gs_cf = np.array((426.08,370.98,365.04,366.62))*m+b
 xu_gs_cf_sigma = np.array((435.59,378.51,373.36,380.89))*m+b
 xu_gs_o3 = np.array((416.56,373.76,356.71,358.30))*m+b
@@ -62,48 +79,50 @@ for i in range(4):
         cuo.append(tmp)
 # Reshape
 cuo = np.reshape(np.array(cuo),(4,10000))
-
-
-# Plot it
-fig1 = plt.figure(1)
-
 for i in range(4):
-    ax = plt.subplot(2,2,i+1)
-    ax.hist(ceo3_day[i])
-    #ax.set_xlim(240,360)
-    ax.set_ylim(0,40)
-    ax.set_title(xu_o3_label[i])
-    ax.text(0.05, 0.95, "(%3.0f $\pm$%1.0f) $ppb \cdot h$" % (ceo3_day_mean[i], ceo3_day_sigma[i]), transform=ax.transAxes)
+    print("%s (%1.2f +- %1.2f) mmol m^-2"  % (xu_o3_label[i], np.around(cuo[i].mean(),2), cuo[i].std()))
 
-fig1.axes[3].set_xlabel("$\Sigma_{10h} O_3 (ppb \cdot h)$ ", x=-0.15)
+if (False):
+    # Plot it
+    fig1 = plt.figure(1)
+
+    for i in range(4):
+        ax = plt.subplot(2,2,i+1)
+        ax.hist(ceo3_day[i])
+        #ax.set_xlim(240,360)
+        ax.set_ylim(0,40)
+        ax.set_title(xu_o3_label[i])
+        ax.text(0.05, 0.95, "(%3.0f $\pm$%1.0f) $ppb \cdot h$" % (ceo3_day_mean[i], ceo3_day_sigma[i]), transform=ax.transAxes)
+
+    fig1.axes[3].set_xlabel("$\Sigma_{10h} O_3 (ppb \cdot h)$ ", x=-0.15)
 
 
-fig2 = plt.figure(2)
+    fig2 = plt.figure(2)
 
-for i in range(4):
-    ax = plt.subplot(2,2,i+1)
-    ax.hist(ceo3[i])
-    #ax.set_xlim(7000,14000)
-    ax.set_ylim(0,40)
-    ax.set_title(xu_o3_label[i])
+    for i in range(4):
+        ax = plt.subplot(2,2,i+1)
+        ax.hist(ceo3[i])
+        #ax.set_xlim(7000,14000)
+        ax.set_ylim(0,40)
+        ax.set_title(xu_o3_label[i])
     
-    ax.text(0.05, 0.95, "(%5.0f $\pm$%2.0f) $ppb \cdot h \cdot d$" % (np.around(ceo3[i].mean(),-2), ceo3[i].std()), transform=ax.transAxes)
+        ax.text(0.05, 0.95, "(%5.0f $\pm$%2.0f) $ppb \cdot h \cdot d$" % (np.around(ceo3[i].mean(),-2), ceo3[i].std()), transform=ax.transAxes)
 
-fig2.axes[3].set_xlabel("$\Sigma_{10h} O_3 (ppb \cdot h \cdot d)$ ", x=-0.15)
+    fig2.axes[3].set_xlabel("$\Sigma_{10h} O_3 (ppb \cdot h \cdot d)$ ", x=-0.15)
 
-fig3 = plt.figure(3)
+    fig3 = plt.figure(3)
+        
+    for i in range(4):
+        ax = plt.subplot(2,2,i+1)
+        ax.hist(cuo[i])
+        #ax.set_xlim(7000,14000)
+        ax.set_ylim(0,400)
+        ax.set_title(xu_o3_label[i])
+        ax.text(0.05, 0.95, "(%1.2f $\pm$%1.2f) $mmol m^{-2}$" % (np.around(cuo[i].mean(),2), cuo[i].std()), transform=ax.transAxes)
 
-for i in range(4):
-    ax = plt.subplot(2,2,i+1)
-    ax.hist(cuo[i])
-    #ax.set_xlim(7000,14000)
-    ax.set_ylim(0,400)
-    ax.set_title(xu_o3_label[i])
-    ax.text(0.05, 0.95, "(%1.2f $\pm$%1.2f) $mmol m^{-2}$" % (np.around(cuo[i].mean(),2), cuo[i].std()), transform=ax.transAxes)
+    fig3.axes[3].set_xlabel("CUO $(mmol\,m^{-2}$) ", x=-0.15)
 
-fig3.axes[3].set_xlabel("CUO $(mmol\,m^{-2}$) ", x=-0.15)
-
-# Show it
-plt.show(block=False)
+    # Show it
+    plt.show(block=False)
 
     
