7bee2696974bd6404be2da63b827612100466b6a
ziu1986 <Stefanie.Falk@geo.uio.no>
2020-03-26 17:33:04 +0100

    Design update for ozone and heatwave paper.

--- sun_hours/test_fennoscandia.py
+++ sun_hours/test_fennoscandia.py
@@ -1,23 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from sun_hours import *
-from mytools.met_tools import plot_month_span, plot_month_name, print_all
+from mytools.plot_tools import plot_month_span, plot_month_name, print_all
+from mytools.station_info import station_location
 from cycler import cycler
 
-station_labels = ("Jergul", "Karasjok", "Svanvik", "Pallas", "Esrange")
-day_length_stations = [ daylength(doy, lat) for doy in range(1,366) for lat in (69.45,69.467,69.45,69.97,67.83)]
-day_length_stations = np.array(day_length_stations).reshape(365,5)
+station_labels = ("Jergul", "Karasjok", "Svanvik", "Pallas", "Esrange", "Prestebakke")
+day_length_stations = [ daylength(doy, station_location[each].lat) for doy in range(1,366) for each in station_labels]
+day_length_stations = np.array(day_length_stations).reshape(365,len(station_labels))
 
 # Plot it
 plt.close("all")
+custom_cycler = (cycler(color=['orange', 'orange', 'blueviolet', 'black', 'blue', 'red']) +
+cycler(linestyle=['-', '--', ':', '-.', '-', '--']))
 
 fig1 = plt.figure(1, figsize=(9,6))
 fig1.canvas.set_window_title("test_fennoscandia")
 ax11 = plt.subplot()
-
+ax11.set_prop_cycle(custom_cycler)
 plots = ax11.plot(day_length_stations)
-ax11.set_xlabel("Day of Year")
-ax11.set_ylabel("Day Length (hours)")
+ax11.set_xlabel("Time (day of year)")
+ax11.set_ylabel("Day length (h)")
+
 ax11.legend(plots, station_labels)
 plot_month_span(ax11)
 plot_month_name(ax11, ypos=24.5)
