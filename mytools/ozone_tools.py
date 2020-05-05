649cb5423f1651db392bc8c0c7666efad82b81d1
ziu1986 <Stefanie.Falk@geo.uio.no>
2020-05-05 14:06:20 +0200

    Added extended functions to compute ozone burden from monthly mean in OsloCTM3.

--- mytools/ozone_tools.py
+++ mytools/ozone_tools.py
@@ -186,5 +186,120 @@ def flunder(x, **kwarg):
             result.append(elem)
     return(np.array(result))
 
+def get_molarweight(data):
+    '''
+    Extract dictonary of all tracers molarweights from OsloCTM3 file.
+    Parameters
+    ----------
+    data : xarray
+        Source OsloCTM3 avgsav file.
+    Returns
+    -------
+    tracer_molweight : a dictionary with corresponding molmasses.
+    '''
+    tracer_names = np.squeeze(data['tracer_name'].data)          # Extract the names of the tracers
+    tracer_names = [name.strip() for name in tracer_names]       # Trim whitespace
+    tracer_molweight = np.squeeze(data['tracer_molweight'].data) # Extract the molarweight
+    return(xr.DataArray(tracer_molweight, coords=[('name', tracer_names)])) # Return DataArray
+
+def get_vmr(data, **karg):
+    '''
+    Compute volume mixing ratio (VMR) from mass mixing ratio (MMR).
+    Parameters
+    ----------
+    data : xarray
+        Source OsloCTM3 avgsav file.
+    Keyword arguments
+    -----------------
+    tracer : string
+        Valid name of a valid tracer.
+    unit : string
+        Traget unit for the conversion.
+        Standard: mol/mol
+        Option: ppm, ppb, ppt
+    Mtracer : float
+        Molecular mass of the tracer.
+        Standard: infer from data.
+        Option: user defined number.
+    mair : float
+        Molecular density of air
+        Standard: use field 'AIR' in data
+        Option: user defined input.
+    '''
+    tracer = karg.pop('tracer', 'none')
+    unit = karg.pop('unit', 'mol/mol')
+    Mtracer = karg.pop('molw',  get_molarweight(data).sel(name=tracer)) # g/mol
+    mair =  karg.pop('mair', data['AIR']) # g/cm3
+    Mair = 28.949 # g/mol
+    mtracer = data[tracer] # g/cm3
+    
+    vmr = mtracer/mair*Mair/Mtracer
+    if unit=='ppm':
+        vmr = vmr*10**6
+    elif unit=='ppb':
+        vmr = vmr*10**9
+    elif unit=='ppt':
+        vmr = vmr*10**12
+    else:
+        unit = 'mol/mol'    # Fall back to default
+    vmr.attrs['units'] = unit
+    return(vmr)
+
+
+
+def compute_column_density(press, atm_var, **kargs):
+    '''
+    Compute the column density.
+    Parameters
+    ----------
+    press : xarray
+        The atmospheric pressure field in hPa!
+    atm_var : xarray
+        The atmospheric tracer field in mol/mol.
+    Keyword arguments
+    -----------------
+    unit : sting
+        Define the unit for the output.
+        Standard: molecules/cm3
+        Option: DU
+    accumulate : boolean
+        Accumulate all levels.
+        Standard: True
+    Returns
+    -------
+    atm_new : xarray
+        Global field of the tracer.    
+    '''
+    # Keyword arguments
+    unit = kargs.pop('unit', 'molecules/cm2')
+    b_accu = kargs.pop('accumulate', True)
+    # Conversion factors
+    Mdry = 0.0289645    # molec. wt. of dry air, kg/mol
+    constant = N_A/(g*Mdry) # molecules*s2/(m*kg)
+    cm2 = 1e-4
+    DU = 1e-2/2.69e16
+    # Computation
+    # If press.data would not be used, labels may interfer in regrouping.
+    # Shift the pressure field and multiply it with the unshifted atmospheric variable,
+    # but start with second and end one before last.
+    p1 = press[2:]
+    p2 = press[:-2]
+    if np.all(np.equal(data['O3'].shape, data['lev'].shape)):
+        atm_new = 0.5*(p1-p2)*atm_var[1:-1]
+    else:
+        atm_new = xr.DataArray.copy(atm_var)
+        for i in range(len(p1)):
+            atm_new[:,i,:,:] = np.fabs(0.5*(p1[i]-p2[i]))*atm_var[:,i+1,:,:]
+    # Sum it up == integration
+    if b_accu:
+        atm_new = atm_new.sum(dim='lev')
+    if unit == 'DU':
+        atm_new = atm_new*constant*DU
+    else:
+        atm_new = atm_new*constant*cm2
+    # Set the new units
+    atm_new.attrs['units'] = unit
+    return atm_new
+
 
 
