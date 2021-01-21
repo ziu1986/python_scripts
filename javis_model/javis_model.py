761e23dbd2644f378f2d7ff70bbf76c68229c21e
Stefanie Falk <Stefanie.Falk@geo.uio.no>
2021-01-21 16:08:55 +0100

    Added class implementation and dummy functions.

--- javis_model/javis_model.py
+++ javis_model/javis_model.py
@@ -1,161 +1,190 @@
-def f_temp(temperature, **karg):
-    '''
-    Compute Javis f_temp function.
-    Parameters
-    ----------
-    temperature : float
+class JavisModel:
+    def __init__(self, species, **karg):
+        self.name = species
+        self.temp_min = karg.pop("Tmin")
+        self.temp_max = karg.pop("Tmax")
+        self.temp_opt = karg.pop("Topt")
+        self.f_min = karg.pop("fmin")
+        self.vpd_min = karg.pop("Dmin")
+        self.vpd_max = karg.pop("Dmax")
+        self.alpha_light = karg.pop("alpha")
+        self.gmax = karg.pop("gmax")
+
+
+    def f_temp(self, temperature):
+        '''
+        Compute Javis f_temp function.
+        Parameters
+        ----------
+        temperature : float
         The 2m temperature in deg C
-    Keyword arguments
-    -----------------
-    Tmin : float
+        Keyword arguments
+        -----------------
+        Tmin : float
         Minimum temperature for stomatal opening in deg C
-    Tmax : float
+        Tmax : float
         Maximum temperature for stomatal opening in deg C
-    Topt : float
+        Topt : float
         Optimal temperature for stomatal opening in deg C
-    Returns
-    -------
-    f_temp : float, range 0,1
-    '''
-    import numpy as np
+        Returns
+        -------
+        f_temp : float, range 0,1
+        '''
+        import numpy as np
        
-    TMIN = karg.pop("Tmin")
-    TMAX = karg.pop("Tmax")
-    TOPT = karg.pop("Topt")
+        TMIN = self.temp_min
+        TMAX = self.temp_max
+        TOPT = self.temp_opt
 
-    beta = ((TMAX-TOPT)/(TOPT-TMIN))
-    f_temp = (temperature-TMIN)/(TOPT-TMIN)*((TMAX-temperature)/(TMAX-TOPT))**beta
+        beta = ((TMAX-TOPT)/(TOPT-TMIN))
+        f_temp = (temperature-TMIN)/(TOPT-TMIN)*((TMAX-temperature)/(TMAX-TOPT))**beta
 
-    # Catch negative values
-    try:
-        f_temp[np.where(f_temp<0)] = 0
-    except TypeError:
-        if f_temp < 0:
-            f_temp = 0
-    # Catch values larger 1
-    try:
-        f_temp[np.where(f_temp>1)] = 1
-    except TypeError:
-        if f_temp > 1:
-            f_temp = 1
-        
-    return(f_temp)
+        # Catch negative values
+        try:
+            f_temp[np.where(f_temp<0)] = 0
+        except TypeError:
+            if f_temp < 0:
+                f_temp = 0
+        # Catch values larger 1
+        try:
+            f_temp[np.where(f_temp>1)] = 1
+        except TypeError:
+            if f_temp > 1:
+                f_temp = 1
 
-def f_vpd(VPD, **karg):
-    '''
-    Compute Javis f_vpd function.
-    Parameters
-    ----------
-    VPD : float
+        return(f_temp) 
+
+    def f_vpd(self, vpd):
+        '''
+        Compute Javis f_vpd function.
+        Parameters
+        ----------
+        VPD : float
         Water vapor pressure deficit in kPa
-    Keyword arguments
-    -----------------
-    fmin : float
+        Keyword arguments
+        -----------------
+        fmin : float
         Minimum value for stomatal opening
-    Dmin : float
+        Dmin : float
         Maximum VPD for stomatal opening in KPa
-    Dmax : float
+        Dmax : float
         Maximum VPD for stomatal opening in kPa
-    Returns
-    -------
-    f_vpd : float, range 0,1
-    
-    '''
-    import numpy as np
-    
-    FMIN = karg.pop("fmin")
-    DMIN = karg.pop("Dmin")
-    DMAX = karg.pop("Dmax")
-
-    f_vpd = FMIN + (1-FMIN) * (DMIN-VPD)/(DMIN-DMAX)
-
-    # Catch negative values
-    try:
-        f_vpd[np.where(f_vpd<0)] = 0
-    except TypeError:
-        if f_vpd < 0:
-            f_vpd = 0
-    # Catch values larger 1
-    try:
-        f_vpd[np.where(f_vpd>1)] = 1
-    except TypeError:
-        if f_vpd > 1: 
-            f_vpd = 1
-         
-    return(f_vpd)
-
-
-def f_light(ppfd, **karg):
-    '''
-    Compute Javis f_light function.
-    Parameters
-    ----------
-    ppfd : float
-        Photoactive photonflaux density in W/m^2/s
-    Keyword arguments
-    -----------------
-    alpha : float
-        slope parameter in m^2*s/W
-    Returns
-    -------
-    f_light : float, range 0,1
-    
-    '''
-
-    import numpy as np
-    
-    ALPHA = karg.pop("alpha")
-
-    f_light = 1 - np.exp(-ALPHA*ppfd)
-
-    # Catch negative values
-    try:
-        f_light[np.where(f_light<0)] = 0
-    except TypeError:
-        if f_light < 0:
-            f_light = 0
-    # Catch values larger 1
-    try:
-        f_light[np.where(f_light>1)] = 1
-    except TypeError:
-        if f_light >1:
-            f_light = 1
+        Returns
+        -------
+        f_vpd : float, range 0,1
         
-    return(f_light)
+        '''
+        import numpy as np
+        
+        FMIN = self.f_min
+        DMIN = self.vpd_min
+        DMAX = self.vpd_max
+
+        f_vpd = FMIN + (1-FMIN) * (DMIN-vpd)/(DMIN-DMAX)
+
+        # Catch negative values
+        try:
+            f_vpd[np.where(f_vpd<0)] = 0
+        except TypeError:
+            if f_vpd < 0:
+                f_vpd = 0
+        # Catch values larger 1
+        try:
+            f_vpd[np.where(f_vpd>1)] = 1
+        except TypeError:
+            if f_vpd > 1: 
+                f_vpd = 1
+
+        return(f_vpd)
 
 
-def stomatal_conductance(f_phen, f_light, f_min, f_temp, f_vpd, f_sw, gmax):
-    '''
-    Compute Javis stomatal conductance.
-    Parameters
-    ----------
-    f_phen : float
+    def f_light(self, ppfd):
+        '''
+        Compute Javis f_light function.
+        Parameters
+        ----------
+        ppfd : float
+        Photoactive photonflaux density in W/m^2/s
+        Keyword arguments
+        -----------------
+        alpha : float
+        slope parameter in m^2*s/W
+        Returns
+        -------
+        f_light : float, range 0,1
+        
+        '''
 
-    f_light : float
+        import numpy as np
+        
+        ALPHA = self.alpha_light
 
-    f_min : float
+        f_light = 1 - np.exp(-ALPHA*ppfd)
+        
+        # Catch negative values
+        try:
+            f_light[np.where(f_light<0)] = 0
+        except TypeError:
+            if f_light < 0:
+                f_light = 0
+        # Catch values larger 1
+        try:
+            f_light[np.where(f_light>1)] = 1
+        except TypeError:
+            if f_light >1:
+                f_light = 1
 
-    f_temp : float
-
-    f_vpd : float
-
-    f_sw : float
-
-    Arguments
-    ---------
-    gmax : float
-        Maximum stomatal conductance
-    Returns
-    -------
-    gsto : float
-        Stomatal conductance
-
-    '''
-    import numpy as np
-    gsto = gmax * f_phen * f_light * np.maximum(f_min, f_temp, f_vpd, f_sw)
-
-    return(gsto)
+        return(f_light)
     
+    def f_sw(self):
+        '''
+        Compute the soilwater stress by plant available water (PAW) method
+        Parameters
+        ----------
+        FC : float
+            Soil caracteristics
+        PAW : float
+            plant available water
+        Returns
+        -------
+        f_sw : float
+
+        '''
+        return(0)
+        
+    def stomatal_conductance(self, temperature, vpd, ppfd):
+        '''
+        Compute Javis stomatal conductance.
+        Parameters
+        ----------
+        f_phen : float
+            
+        f_light : float
+        
+        f_min : float
+        
+        f_temp : float
+        
+        f_vpd : float
+        
+        f_sw : float
+            
+        Arguments
+        ---------
+        gmax : float
+        Maximum stomatal conductance
+        Returns
+        -------
+        gsto : float
+            Stomatal conductance
+
+        '''
+        import numpy as np
+            
+        gsto = self.gmax * self.f_phen * self.f_light(ppfd) * np.maximum(self.f_min, self.f_temp(temperature), self.f_vpd(vpd), self.f_sw())
+
+        return(gsto)
+
 
 
 
