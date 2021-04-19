2559cb6e2f36563bdd2f4c5e19b1c96fdd96c5bc
Stefanie Falk <Stefanie.Falk@geo.uio.no>
2021-04-19 15:44:40 +0200

    Add letter cycling

--- mytools/ozone_tools.py
+++ mytools/ozone_tools.py
@@ -454,3 +454,8 @@ def rms(observation, prediction, **karg):
     weights_sum = np.nansum(weights)
     
     return np.sqrt(np.nansum(weights*(prediction-observation)**2)/weights_sum)
+
+def char_range(c1, c2):
+    """Generates the characters from `c1` to `c2`, inclusive."""
+    for c in xrange(ord(c1), ord(c2)+1):
+        yield chr(c)
