9ec17fecd65d5092af5c65f21b4034fe91d42c23
ziu1986 <stefanie.falk@lmu.de>
2021-10-24 18:20:16 +0200

    Add brute force mc method to find best topic distri

---
+++ brute_force/brute_force.py
@@ -0,0 +1,21 @@
+import random
+import numpy as np 
+
+n = 22
+probe = 1000000
+penalty = 4*n
+
+test = [np.array(random.sample(range(1,n+1), k=n)) for i in range (3)]
+
+
+for i in range(probe):
+    weight = np.repeat(4, n)
+    assign = np.array(random.sample(range(1,n+1), k=n))
+    for i, itest in zip(np.arange(3,0,-1), test[::-1]):
+        weight[np.where(itest==assign)] = i
+    if weight.sum() < penalty:
+        penalty = weight.sum()
+        result = assign
+
+print(penalty, result)
+
