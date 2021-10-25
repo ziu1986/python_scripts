7166b56e329462c7a8528f2a51b472c7fd0f6c22
ziu1986 <stefanie.falk@lmu.de>
2021-10-25 13:33:03 +0200

    Remove legacy

--- brute_force/brute_force.py
+++ brute_force/brute_force.py
@@ -4,13 +4,9 @@ import pandas as pd
 
 n = 22
 kategories = 22
-probe = 100
+probe = 10000
 penalty = 4*n
 
-#test = [np.array(random.sample(range(1,n+1), k=n)) for i in range (3)]
-#test = [(19, 10, 17, 4, 19, 15, 17), 
-#        (17, 5, 15, 2, 4, 16, 10),
-#        (20, 7, 18, 1, 9, 17, 4)]
 test_src = pd.read_csv("preferences.txt")
 test = [test_src['Pref%d' % i].to_numpy() for i in range(1,4)]
 
