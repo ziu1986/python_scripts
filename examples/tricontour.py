2c5591ea4b7c66d246e0fc26f7a01a30b234ad70
ziu1986 <stefanie.falk@lmu.de>
2021-11-10 16:45:30 +0100

    Refine plot

--- examples/tricontour.py
+++ examples/tricontour.py
@@ -1,9 +1,9 @@
-
 import os, sys
 import numpy as np
 import xarray as xr
 import matplotlib.pyplot as plt
 
+plt.close('all')
 src = os.environ['MODELS'] + "/icon/build_lndonly/experiments/jsbalone_R2B4_sfa/jsbalone_R2B4_sfa_lnd_basic_ml_20000201T000000Z.nc"
 data = xr.open_dataset(src)
 
@@ -11,8 +11,14 @@ x = data['clon'].data
 y = data['clat'].data
 lai = data['pheno_lai_veg'].isel(time=0).data
 
-f, ax = plt.subplots(1,2, sharex=True, sharey=True)
+f, ax = plt.subplots(2,1, sharex=True, sharey=True)
 ax[0].tripcolor(x,y,lai)
-ax[1].tricontourf(x,y,lai, 20) # choose 20 contour levels, just to show how good its interpolation is
+img2 = ax[1].tricontourf(x,y,lai, 20, cmap=plt.cm.gist_earth_r) # choose 20 contour levels, just to show how good its interpolation is
+
+ax[1].set_xlabel("Longitude (au)")
+ax[1].set_ylabel("Latitude (au)")
+
+cbar = plt.colorbar(img2, ax=ax[1])
+cbar.set_label("LAI")
 
 plt.show(block=False)
