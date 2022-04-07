f470b09f39ec99d64c2ec81320d9d76569edf652
ziu1986 <stefanie.falk@lmu.de>
2022-04-07 13:13:56 +0200

    Increase lable size and change plot ratios

--- Svanvik/clim_anomalies/plot_climate_diagram.py
+++ Svanvik/clim_anomalies/plot_climate_diagram.py
@@ -6,14 +6,14 @@ import matplotlib.pyplot as plt
 from mytools.plot_tools import *
 from mytools.station_info import *
 
-def plot_climadiagram(temp, precip, **kwarg):
+def plot_climate_diagram(temp, precip, **kwarg):
     bfrost = kwarg.pop('frost', False)
     bdrought = kwarg.pop('drought', False)
     verbose = kwarg.pop('verbose', False)
     temp_err = kwarg.pop('temp_err', None)
     precip_err = kwarg.pop('precip_err', None)
 
-    fig1 = plt.figure(1)
+    fig1 = plt.figure(1, figsize=(12,8))
     fig1.subplots_adjust(right=0.9)
     fig1.canvas.set_window_title("climatediagram")
     ax11 = plt.subplot()
@@ -33,10 +33,10 @@ def plot_climadiagram(temp, precip, **kwarg):
 
     if precip_err is None:
         precip_max = np.ceil(precip.values.max())
-        ax11.text(9, precip_max*0.5-5, "$_{max}: %2.1f\,mm$\n$_{min}: %2.1f\,mm$" % (precip.max(), precip.min()), color='blue', size=14)
+        ax11.text(8.25, precip_max*0.5-6.75, "$Precip_{max}: %2.1f\,mm$\n$Precip_{min}: %2.1f\,mm$" % (precip.max(), precip.min()), color='blue', size=16)
     else:
         precip_max = np.ceil(precip.values.max()+precip_err.max())
-        ax11.text(8.5, precip_max*0.5-7.5, "$_{max}: (%2.1f\pm %2.1f)\,mm$\n$_{min}: (%2.1f\pm %2.1f)\,mm$\n$\Sigma: (%2.1f\pm %2.1f)\,mm$" % (precip.max(), precip_err[np.where(precip==precip.max())[0]], precip.min(), precip_err[np.where(precip==precip.min())[0]], precip.sum(), np.sqrt((precip_err**2).sum())), color='blue', size=14)
+        ax11.text(7.75, precip_max*0.5-9.25, "$Precip_{max}: (%2.1f\pm %2.1f)\,mm$\n$Precip_{min}: (%2.1f\pm %2.1f)\,mm$\n$\Sigma: (%2.1f\pm %2.1f)\,mm$" % (precip.max(), precip_err[np.where(precip==precip.max())[0]], precip.min(), precip_err[np.where(precip==precip.min())[0]], precip.sum(), np.sqrt((precip_err**2).sum())), color='blue', size=14)
 
 
     temp_max = np.ceil(temp.max())
@@ -45,16 +45,16 @@ def plot_climadiagram(temp, precip, **kwarg):
     if temp_err is not None:
         temp_max = np.ceil(temp_max+temp_err.max())
         temp_min = np.floor(temp_min-temp_err.max())
-        ax11.text(0, precip_max*0.5-6., "$_{max}: (%2.1f\pm %2.1f)^\circ C$\n$_{min}: (%2.1f\pm %2.1f)^\circ C$" % (temp.max(), temp_err[np.where(temp==temp.max())[0]], temp.min(), temp_err[np.where(temp==temp.min())[0]]), color='red', size=14)
+        ax11.text(0, precip_max*0.5-7.5, "$T_{max}: (%2.1f\pm %2.1f)^\circ C$\n$T_{min}: (%2.1f\pm %2.1f)^\circ C$" % (temp.max(), temp_err[np.where(temp==temp.max())[0]], temp.min(), temp_err[np.where(temp==temp.min())[0]]), color='red', size=14)
     else:
-        ax11.text(0, precip_max*0.5-5, "$_{max}: %2.1f^\circ C$\n$_{min}: %2.1f^\circ C$" % (temp_max, temp_min), color='red', size=14)
+        ax11.text(0, precip_max*0.5-6.5, "$T_{max}: %2.1f^\circ C$\n$T_{min}: %2.1f^\circ C$" % (temp_max, temp_min), color='red', size=16)
     
     if verbose:
         print(temp_max, temp_min, precip_max)
         print(np.min((0,temp_min)), np.max((temp_max, precip_max*0.5)))
         
     ax11.set_ylim(np.min((0,temp_min)), np.max((temp_max, precip_max*0.5)))
-    ax11.set_xticklabels([get_month_name(imonth, length=3) for imonth in np.arange(1,13)])
+    ax11.set_xticklabels([get_month_name(imonth, length=3) for imonth in np.arange(1,13)], size=18)
     ax11.set_ylabel("Temperature ($^\circ\,C$)", color='red')
     ax12.set_ylabel("Precipitation (mm)", color='blue')
     ax12.set_ylim(np.min((0,temp_min*2)), np.max((temp_max*2, precip_max)))
@@ -63,7 +63,7 @@ def plot_climadiagram(temp, precip, **kwarg):
         frost = np.where(temp<=0)[0]
         if verbose:
             print(frost+1)
-        ax11.scatter(frost, np.zeros_like(frost), marker='*', s=12**2, label='frost months')
+        ax11.scatter(frost, np.zeros_like(frost), marker='*', s=16**2, label='frost months')
 
     if bdrought:
         drought = np.where(precip<temp*2)[0]
@@ -71,13 +71,13 @@ def plot_climadiagram(temp, precip, **kwarg):
             print(drought+1, temp[drought])
             ax11.fill_between(drought, 0, temp[drought], color='black')
     if bfrost or bdrought:
-        ax11.legend()
+        ax11.legend(loc='lower center', prop={'size': 14})
 
 # Clean up
 plt.close('all')
 
-src_svanvik_clim = os.environ['DATA']+'/astra_data/observations/metdata_svanvik/temp_svanvik*'
-src_svanvik_prec_clim = os.environ['DATA']+'/astra_data/observations/metdata_svanvik/precip_svanvik_1992_2020.csv'
+src_svanvik_clim = os.environ['DATA']+'/astra/observations/metdata_svanvik/temp_svanvik*'
+src_svanvik_prec_clim = os.environ['DATA']+'/astra/observations/metdata_svanvik/precip_svanvik_1992_2020.csv'
 
 data_list = []
 try:
@@ -101,11 +101,12 @@ except NameError:
 # Plot it
 temperature = data_svanvik_clim.iloc[:,0].groupby(data_svanvik_clim.index.month)
 precipitation = (data_svanvik_precip_clim[:'2012'].groupby(['year','month']).sum()).groupby(['month'])
-plot_climadiagram(temperature.mean().values,
+plot_climate_diagram(temperature.mean().values,
                   precipitation.mean()['RR'],
-                  temp_err=temperature.std().values/np.sqrt(data_svanvik_clim.index.year.unique().size),
-                  precip_err=precipitation.std()['RR'].values/np.sqrt(data_svanvik_precip_clim.index.year.unique().size),
+                  temp_err=temperature.std().values, 
+                  #precip_err=precipitation.std()['RR'].values, 
                   verbose=True, frost=True)
-#
+##/np.sqrt(data_svanvik_clim.index.year.unique().size), 
+##/np.sqrt(data_svanvik_precip_clim.index.year.unique().size),
 # Show it
 plt.show(block=False)
