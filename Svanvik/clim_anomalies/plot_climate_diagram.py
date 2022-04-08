df5d237b68e0143f35c5ff1c58190460692fdac8
ziu1986 <stefanie.falk@lmu.de>
2022-04-08 14:17:49 +0200

    Split plotting function; add plot for ozone

--- Svanvik/clim_anomalies/plot_climate_diagram.py
+++ Svanvik/clim_anomalies/plot_climate_diagram.py
@@ -5,6 +5,7 @@ import xarray as xr
 import numpy as np
 import matplotlib.pyplot as plt
 from mytools.plot_tools import *
+from mytools.ozone_tools import *
 from mytools.station_info import *
 
 def plot_climate_diagram(temp, precip, **kwarg):
@@ -75,43 +76,73 @@ def plot_climate_diagram(temp, precip, **kwarg):
     if bfrost or bdrought:
         ax11.legend(loc='lower center', prop={'size': 14})
 
-def plot_climate_diagram_box(temp, precip, **kwarg):
+def make_boxplot(var, ax, **kwarg):
+    color = kwarg.pop('color', 'black')
+    notch = kwarg.pop('notch', False)
+
+    box_var = var.boxplot(ax=ax, by='month', showmeans=True,
+                notch=notch, bootstrap=1000, return_type='dict')
+ 
+    [[item.set_color(color) for item in box_var[key]['medians']] for key in box_var.keys()]
+    [[item.set_markerfacecolor(color) for item in box_var[key]['means']] for key in box_var.keys()]
+    [[item.set_linewidth(4)for item in box_var[key]['medians']] for key in box_var.keys()]
+
+    return(box_var)
+    
+
+def plot_climate_diagram_box(temp, precip, rad, **kwarg):
 
     notch = kwarg.pop('notch', False)
 
-    fig1 = plt.figure(figsize=(16,6))
+    fig1 = plt.figure(figsize=(20,6))
     fig1.canvas.set_window_title("climatediagram_box")
-    ax11 = plt.subplot(121)
-    ax12 = plt.subplot(122)
+    
+    ax11 = plt.subplot(131)
+    ax12 = plt.subplot(132)
+    ax13 = plt.subplot(133)
 
-    box_temp = temp.boxplot(ax=ax11, by='month', showmeans=True,
-                notch=notch, bootstrap=1000, return_type='dict')
-    box_precip = precip.boxplot(ax=ax12, by='month', showmeans=True,
-                notch=notch, bootstrap=1000, return_type='dict')
+    box_temp = make_boxplot(temp,ax11,notch=notch, color='r')
+    box_precip = make_boxplot(precip,ax12,notch=notch, color='b')
+    box_rad = make_boxplot(rad,ax13,notch=notch, color='black')
 
     
-    [[item.set_color('r') for item in box_temp[key]['medians']] for key in box_temp.keys()]
-    [[item.set_markerfacecolor('r') for item in box_temp[key]['means']] for key in box_temp.keys()]
-    [[item.set_linewidth(4)for item in box_temp[key]['medians']] for key in box_temp.keys()]
-   
-    
-    [[item.set_color('b') for item in box_precip[key]['medians']] for key in box_precip.keys()]
-    [[item.set_markerfacecolor('b') for item in box_precip[key]['means']] for key in box_precip.keys()]
-    [[item.set_linewidth(4)for item in box_precip[key]['medians']] for key in box_precip.keys()]
-
-    for ax, ititle in zip([ax11,ax12], ["(a)", "(b)"]):
-        ax.set_xticklabels([get_month_name(imonth, length=3) for imonth in np.arange(1,13)], size=18)
-        ax.set_title(ititle,y=-0.18,size=18)
+    for ax, ititle in zip(fig1.axes, ["(a)", "(b)", "(c)"]):
+        ax.set_xticklabels([get_month_name(imonth, length=3) for imonth in np.arange(1,13)], size=16)
+        ax.set_title(ititle, size=18) #y=-0.18
         ax.set_xlabel("")
 
     ax11.set_ylabel("Temperature ($^\circ\,C$)")
     ax12.set_ylabel("Precipitation (mm)")
+    ax13.set_ylabel("Gobal radiation ($W\,m^{-2}$)")
+
+    plt.suptitle('')
+    plt.tight_layout()
+
+def plot_climate_diagram_ozone_box(ozone, **kwarg):
+    notch = kwarg.pop('notch', False)
+
+    fig1 = plt.figure(figsize=(10,6))
+    fig1.canvas.set_window_title("climatediagram_ozone_box")
     
+    ax11 = plt.subplot()
+    
+    box_ozone = make_boxplot(ozone,ax11,notch=notch, color='blueviolet')
+
+    ax11.set_xticklabels([get_month_name(imonth, length=3) for imonth in np.arange(1,13)], size=16)
+    ax11.set_xlabel("")
+    ax11.set_title('')
+
+    ax11.set_ylabel("Ozone (ppb)")
+    plt.suptitle('')
+    plt.tight_layout()
+
 # Clean up
 plt.close('all')
 
 src_svanvik_clim = os.environ['DATA']+'/astra/observations/metdata_svanvik/temp_svanvik*'
 src_svanvik_prec_clim = os.environ['DATA']+'/astra/observations/metdata_svanvik/precip_svanvik_1992_2020.csv'
+src_svanvik_rad_clim = os.environ['DATA']+'/astra/observations/metdata_svanvik/svanvik_glob_rad_2000_2017.csv'
+src_svanvik_ozone_clim = os.environ['DATA']+'/astra/observations/ozone/Svanvik/*.nas'
 
 data_list = []
 try:
@@ -125,6 +156,7 @@ except NameError:
         data_list.append(data_svanvik_clim)
         
     data_svanvik_clim = pd.concat(data_list)
+
     data_svanvik_precip_clim = pd.read_csv(src_svanvik_prec_clim)
     data_svanvik_precip_clim.index = pd.date_range("%s" % data_svanvik_precip_clim['Time Measured'][0][:-3], "%s" % data_svanvik_precip_clim['Time Measured'].iloc[-1][:-3], freq='D')
     data_svanvik_precip_clim = data_svanvik_precip_clim.drop(columns=['Time Measured'])
@@ -132,10 +164,24 @@ except NameError:
     data_svanvik_precip_clim.loc[:,'month'] = data_svanvik_precip_clim.index.month.values
     data_svanvik_precip_clim.loc[:,'year'] = data_svanvik_precip_clim.index.year.values
 
+    data_svanvik_rad_clim = pd.read_csv(src_svanvik_rad_clim)
+    data_svanvik_rad_clim.index = pd.date_range("%s" % data_svanvik_rad_clim['Time measured'][0][:10], "%s" % data_svanvik_rad_clim['Time measured'].iloc[-1][:-3], freq='H')
+    data_svanvik_rad_clim = data_svanvik_rad_clim.drop(columns=['Time measured'])
+
+    data_svanvik_rad_clim.loc[:,'month'] = data_svanvik_rad_clim.index.month.values
+    data_svanvik_rad_clim.loc[:,'year'] = data_svanvik_rad_clim.index.year.values
+
+    data_svanvik_ozone_clim = pd.DataFrame({'ozone':load_data(src_svanvik_ozone_clim)})
+
+    data_svanvik_ozone_clim.loc[:,'month'] = data_svanvik_ozone_clim.index.month.values
+    data_svanvik_ozone_clim.loc[:,'year'] = data_svanvik_ozone_clim.index.year.values
+
+
+
 # Plot it
 temperature = data_svanvik_clim.iloc[:,0].groupby(data_svanvik_clim.index.month)
-
 precipitation = (data_svanvik_precip_clim[:'2012'].groupby(['year','month']).sum())
+
 plot_climate_diagram(temperature.mean().values,
                   precipitation.groupby(['month']).mean()['RR'],
                   temp_err=temperature.std().values, 
@@ -146,6 +192,18 @@ plot_climate_diagram(temperature.mean().values,
 
 tmp_temp = pd.DataFrame({'temp': data_svanvik_clim.iloc[:,0]})
 tmp_temp.loc[:,'month'] = tmp_temp.index.month.values
-plot_climate_diagram_box(tmp_temp, precipitation, notch=True)
+
+tmp_rad = pd.DataFrame({'q0': data_svanvik_rad_clim.iloc[:,0]})
+tmp_rad.loc[:,'month'] = tmp_rad.index.month.values
+
+tmp_ozone = pd.DataFrame({'ozone': data_svanvik_ozone_clim.iloc[:,0]})
+tmp_ozone.loc[:,'month'] = tmp_ozone.index.month.values
+
+
+
+plot_climate_diagram_box(tmp_temp, precipitation, tmp_rad[:'2012'], notch=True)
+
+plot_climate_diagram_ozone_box(tmp_ozone, notch=True)
+
 # Show it
 plt.show(block=False)
